ee5afaa3e0a2138f0d5a02010c571d1ce4361b4c
Mestery <mestery@pm.me>
2021-11-02 16:23:51 +0100

    fix: initialize header to undefined

--- src/index.ts
+++ src/index.ts
@@ -335,7 +335,8 @@ export class Decoder<HeaderReady = false> extends Transform {
 	 * Only available after the `'header'` event
 	 * (and obviously in `'data'`, `'readable'`, `'close'`, and `'end'` events).
 	 */
-	public header!: HeaderReady extends true ? Header : Header | undefined;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public header: HeaderReady extends true ? Header : Header | undefined = undefined as any;
 
 	public constructor({ maxFrameSize, ...transformOptions }: DecoderOptions = {}) {
 		transformOptions.readableObjectMode = true;
